d7bb7ac32c5099095e61bcaff8b5ecea93edc6c8
Mac <mac@akhil.local>
2012-11-01 13:08:04 +0530

    project initailization

---
+++ My-Frist-Game/My-Frist-Game/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  My-Frist-Game
+//
+//  Created by praveen on 01/11/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
